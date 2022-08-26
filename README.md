af98f568e33d9be1376f009c745d1b0954178ddd
MattScop <matteo.scopece@outlook.com>
2022-08-26 08:59:45 +0200

    Add initial algorithm

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # caesar_cipher-encryption
+
+First Ruby Project in the TOP Course. We are asked to create a method which encrypts a word by using the "Caesar Chiper" Encryption.
+
+> SOURCE
+- Link to lesson [HERE]("https://www.theodinproject.com/lessons/ruby-caesar-cipher")
---
+++ script.rb
@@ -0,0 +1,15 @@
+=begin 
+1. Takes string
+2. Turn string into indexed numbers
+    - alphabet in array (index +1)
+    - split string in array
+    - string_array.each |i| aplhabet_array.index_at(string_array[i]+1) 
+3. Shift numbers by X
+    - string_number_array.each |item| item + number
+4. Return the original method mantaining the original case
+    - aplhabet_array.char_at(string_number_array-1)
+=end
+
+def caesar_chiper(string, number)
+    
+end
